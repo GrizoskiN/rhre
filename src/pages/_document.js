79613aed29228b6c909e95e69f98754715ad1329
GrizoskiN <ngrizo@gmail.com>
2024-05-16 13:38:10 +0200

    gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { GoogleTagManager } from '@next/third-parties/google'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head ><meta name="google-site-verification" content="RJjITg1RdbGIKZg38XJzdctv7HzYKYCiXbodV2pj1X0" /></Head>
+      <Head ><meta name="google-site-verification" content="RJjITg1RdbGIKZg38XJzdctv7HzYKYCiXbodV2pj1X0" />
+     
+     
+
+
+      </Head>
+      <GoogleTagManager gtmId="GTM-PL663SLC" />
       <body >
+
         <Main />
         <NextScript />
       </body>
