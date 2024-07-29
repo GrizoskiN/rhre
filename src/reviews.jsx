9dfadd9922707d308cb3a375a73e8eacd9936ba2
GrizoskiN <ngrizo@gmail.com>
2024-07-29 18:49:18 +0200

    testimonials update

--- src/reviews.jsx
+++ src/reviews.jsx
@@ -1,7 +1,7 @@
 const reviews = [
     {
         id:14,
-        name:"Firoz Khan",
+        name:"Firoz K.",
         text: "I know Zaid personally and he becomes like my younger brother. I wanted to take a moment to express my gratitude for the tremendous service Zaid provided during my real estate deal. His professionalism, knowledge, and dedication made the entire process smooth and stress-free. Zaid's expertise in the real estate market was evident, and I truly appreciated his guidance throughout the negotiation and closing phases. Working with Zaid was a pleasure, and I would highly recommend his services to anyone in need of a reliable and trustworthy real estate service provider."
     },
     {id:0,
@@ -9,19 +9,19 @@ const reviews = [
      text:"Overall My Experience With Zaid From Rise High Real Estate In Purchasing A New/off-plan Property Was Exceptional. His Professionalism And Support Were Evident Throughout The Entire Process, As He Guided Us Seamlessly From Start To Finish. He Adeptly Handled Any Challenges That Arose, Ensuring That We Secured The Right Home At The Right Price.      I Have No Hesitation In Recommending Rise High Real Estate, Along With Zaid, To Anyone In Search Of A Property. His Dedication And Expertise Truly Made The Buying Process Smooth And Stress-free. "
     },
     {id:1,
-     name: "Vikram Dhar ",
+     name: "Vikram D. ",
      text:"Impressive And Personalized Services Provided By Zaid Through Out The Process. We Had Nothing To Do And Everything Was Managed By The Rise High Team. Thank You Everyone "
     },
     {id:2,
-     name: "Ahtoh Fiepetypah",
+     name: "Ahtoh F.",
      text:"I Had An Amazing Experience Working With My Real Estate Agent Mr. Zaid. He Is Professional, Knowledgeable, And Dedicated To Helping Me With My Future Home. He Listened To My Needs, Found Property That Matched My Criteria, And Guided Me Through The Entire Process. His Negotiation Skills Were Outstanding, Resulting In A Great Deal. He Was Always Available To Answer My Ids Questions And Made Me Feel Valued. I Highly Recommend Mr. Zaid For Anyone Looking For A Top-notch Real Estate Agent.  "
     },
     {id:3,
-     name: "Sakkeer Hussain",
+     name: "Sakkeer H.",
      text:"I'm Incredibly Pleased With The Service I Received From Mr. Zaid. The Home Buying Process Can Be Daunting, But He Was Very Helpful To Me To Negotiate An Ideal Contract. He Was Always Available And Communicated How Each Part Of The Process Would Move Forward. I Would Like To Thank Zaid Once Again For Working Very Closely With Me With A Personal Touch To Make This Process Very Comfortable. "
     },
     {id:4,
-     name: "Arseny Kosenko",
+     name: "Arseny K.",
      text:"Had A Great Experience With Zaid. Many Thanks To Him. Great Service, A Great Pro Of The Real Estate Market."
     },
     {id:5,
@@ -29,7 +29,7 @@ const reviews = [
      text:"Good And Efficient Communication. Helped Us Agent Zaid Who Shows Great Understanding Even When Issues And Difficulties Appear. Definitely Recommended For Business Aid Especially If You Are Out Of Uae And Want Reliable Partners And Trustees In This Country. "
     },
     {id:6,
-     name: "Muayyad Adili",
+     name: "Muayyad A.",
      text:"Thank you Rise High for putting in the extra efforts during my critical time to select my main life project. Special thanks for Mr. Zaid, he is extremely responsive, fast, clear and straight. His advices are entering the heart directly, he is not searching to sell only, he is searching to help people to get the best option and to keep relationships for a long time. His helpful attitude makes it clear for me to continue to take the decision with few is my consultant for anything I in UAE.  "
     },
     {id:7,
@@ -37,11 +37,11 @@ const reviews = [
      text:"I Know Zaid Personally As A Colleague As A Brother Since 3 Years Now. He Smoothly Did The Sale Of My Villa In Emaar South 4 Bed, I Am Extremely Happy With Over All Experience And Professional Shown By Zaid And His Entire Team. "
     },
     {id:8,
-     name: "Funmi Ajiboye",
+     name: "Funmi A.",
      text:"Zaid Was Highly Professional As Well As Helpful In The Sale Of Our Property In Sur La Mer. Would Recommend Him To Potential Buyers/sellers.  "
     },
     {id:9,
-     name: "Radu Ghita",
+     name: "Radu G.",
      text:"I'm A New Investor In Dubai's Real Estate And I Worked With Zaid To Understand More About The Market And Find The Best Options For Me. I Ended Up With A Unit In Sobha Waves And Zaid Went The Extra Mile To Be Able To Secure That For Me. The Market Is Very Fast And Dynamic If You're Used To Traditional Ones In Europe, So A Resource To Help You Navigate Is Very Helpful.  "
     },
     {id:10,
@@ -57,7 +57,7 @@ const reviews = [
      text:"I Am Extremely Happy With Choosing Rise High, Especially Because Of Zaid. He Went Above And Beyond To Satisfy My Needs. He Was Extremely Professional, Reliable, Fast And Helpful. This Was My First Purchase And It Won't Be My Last! Will Definitely Consult With Him Again. Could Not Recommend Him More Highly! "
     },
     {id:13,
-     name: "Ziqi Ma",
+     name: "Ziqi M.",
      text:"Mr.Zaid is a good agent with great passion and responsibility. Quick and detailed reply all time with friendly manner for the customers all over the world. Thanks for the effort Mr. Zaid have done for us the recent time. ish you a good fortune "
     },
     {
