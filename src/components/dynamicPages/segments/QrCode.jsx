080b10b250b2b557dec39a4ff63d7b07b60611a1
GrizoskiN <ngrizo@gmail.com>
2026-02-14 16:56:57 +0100

    qr

--- src/components/dynamicPages/segments/QrCode.jsx
+++ src/components/dynamicPages/segments/QrCode.jsx
@@ -1,10 +1,10 @@
-import qrimage from "@../../../public/img/qrcode.png"
+
 import Image from "next/image"
 
 const QrCode = ({permit, parts}) => {
   return (
     <div className="">
-      <h3 className=""><span className="font-bold">Permit Number: <a target="_blank" href={parts[4]}><Image src={qrimage} width={150} className=""/></a></span> {permit}</h3>
+      <h3 className=""><span className="font-bold">Permit Number: <a target="_blank" href={parts[4]}><Image src="/img/qrcode.png" width={150} height={150} className=""/></a></span> {permit}</h3>
       {/* {parts[4] && (
   <h3 className="font-semibold text-xl my-3">DLD Permit Number:</h3>
 )}
